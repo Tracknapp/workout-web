890139af1a4ecf11ec731d2ceae770777b2d4f6f
deepak <deepakkumardash313@gmail.com>
2025-10-22 03:50:19 +0530

    Enhance: update authentication middleware to redirect unauthenticated users to sign-in page; improve sign-in page layout

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn fallbackRedirectUrl={"/"} routing="hash" />;
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <SignIn fallbackRedirectUrl={"/"} routing="hash" />
+    </div>
+  );
 }
